1841bc6ec101ba07b57c9122131c6ea92ef2bbb6
Igor <Aleshichev.git@email.ua>
2022-04-26 10:33:32 +0300

    Случайный выбор

---
+++ Random_modul.py
@@ -0,0 +1,10 @@
+import random
+
+random_integer = random.randint(1, 10) # random int 1-10
+print(random_integer)
+
+random_float = random.random()  # random float 0-1
+print(random_float)
+
+randomFloat = random.random() * 5 # random float
+print(randomFloat)
