319ed2d35cf6cf677ea4ff619e8ca6da5122dca3
Igor <Aleshichev.git@email.ua>
2022-05-04 16:04:50 +0300

    черепашка

---
+++ turtle_1.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen     # черепаха, экран
+
+timmy = Turtle()    # создали новый объект
+timmy.shape("turtle")    # методы связанные с объектом
+timmy.color("red")        # методы связанные с объектом
+timmy.forward(100)        # методы связанные с объектом
+my_screen = Screen()     # запускает окно экран
+my_screen.exitonclick()    #программа работает до нажатия кнопки
+
+
+print(my_screen.canvheight)    #точка имени объекта как переменная
